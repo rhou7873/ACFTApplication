4765043916b7b3cc2255ee52b34e418a1449c883
Ryan Hou <rhou7873@gmail.com>
2023-03-03 14:18:58 -0600

    fixed time scoring bug

--- pages/results.tsx
+++ pages/results.tsx
@@ -3,7 +3,7 @@ import NavBar from 'components/NavBar';
 import Card from "@mui/material/Card";
 import { CardContent, Typography } from '@mui/material';
 import Soldier from "types/soldier";
-import { mdl, spt, hrp, sdc} from 'lib/scoreUtilities';
+import { mdl, spt, hrp, sdc, plk, tmr } from 'lib/scoreUtilities';
 
 export default function Results() {
     let [soldierData, setSoldierData] = useState<any[]>([]);
@@ -49,8 +49,8 @@ export default function Results() {
                                     <p>SPT: {soldier.spt + " -> " + spt(soldier, soldier.spt)}</p>
                                     <p>HRP: {soldier.hrp + " -> " + hrp(soldier, soldier.hrp)}</p>
                                     <p>SDC: {soldier.sdc + " -> " + sdc(soldier, soldier.sdc)}</p>
-                                    <p>PLK: {soldier.plk + " -> " + sdc(soldier, soldier.plk)}</p>
-                                    <p>2MR: {soldier.tmr + " -> " + sdc(soldier, soldier.tmr)}</p>
+                                    <p>PLK: {soldier.plk + " -> " + plk(soldier, soldier.plk)}</p>
+                                    <p>2MR: {soldier.tmr + " -> " + tmr(soldier, soldier.tmr)}</p>
                                     <p>Score: {soldier.score}</p>
                                 </Typography>
                             </CardContent>
