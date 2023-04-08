100d6cff6b02c21e67579a7d7f1d1c75402f65ef
Ryan Hou <rhou7873@gmail.com>
2023-04-07 23:08:16 -0500

    cookie bug fix attempt

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -1,14 +1,21 @@
 import { Button } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { getCookie } from "cookies-next";
 
 function HomeButton() {
+  let [role, setRole] = useState("None");
+
   let router = useRouter();
 
   let handleClick = (e: any) => {
-    router.push("/");
+    router.push(`/${role}`);
   }
+
+  useEffect(() => {
+    setRole((getCookie("role") as string).toLowerCase());
+  }, []);
 
   return (
     <div className="homeButtonContainer">
